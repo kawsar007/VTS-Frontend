e3da5803a290cf27499975d17aeefd30fead99cf
kawsar007 <imkawsar007@gmail.com>
2024-09-23 22:20:34 +0600

    Get all data in engine start/stop reports

--- src/pages/reports/engine-start-stop-report.jsx
+++ src/pages/reports/engine-start-stop-report.jsx
@@ -1,9 +1,7 @@
 /* eslint-disable react/prop-types */
-import { BlobProvider, PDFDownloadLink } from "@react-pdf/renderer";
-import { saveAs } from "file-saver";
-import { FiShare2 } from "react-icons/fi";
-import { HiOutlineDownload, HiOutlinePrinter } from "react-icons/hi";
-import DistanceReportDocs from "./download-docs/distance-report-docs";
+import { useEffect, useState } from "react";
+import { formatEpochToDateForTripReport } from "../../utils/select-time-utility";
+import { calculateDuration, formatDuration } from "../../utils/utility";
 
 const EngineStartStopReport = ({
   reports,
@@ -14,32 +12,98 @@ const EngineStartStopReport = ({
   endTime,
   todayFormattedDate,
 }) => {
-console.log(reports);
+  console.log(reports);
+  const [engineStats, setEngineStats] = useState([]);
+  console.log("engineStats", engineStats);
+
+  useEffect(() => {
+    let engineOn = false;
+    let startTime = 0;
+    let stopTime = 0;
+    let distance = 0;
+    let totalSpeed = 0;
+    let speedCount = 0;
+    let maxSpeed = 0;
+
+    const stats = [];
+
+    reports.forEach((entry, index) => {
+      if (entry.engine === 1 && !engineOn) {
+        engineOn = true;
+        startTime = entry.time;
+      }
+
+      if (engineOn && entry.engine === 0) {
+        engineOn = false;
+        stopTime = entry.time;
+
+        // calculate duration
+        const duration = calculateDuration(startTime, stopTime);
+
+        const timeDiff = calculateDuration(reports[index - 1].time, entry.time);
+        distance += reports[index - 1].speed * (timeDiff / 3600);
+
+        // Calculate average speed during engine on
+        const avgSpeed = totalSpeed / speedCount;
+
+        stats.push({
+          startTime: formatEpochToDateForTripReport(startTime),
+          stopTime: formatEpochToDateForTripReport(stopTime),
+          duration: formatDuration(duration),
+          travelledDistance: distance.toFixed(2), // Keep two decimals for distance
+          avgSpeed: avgSpeed.toFixed(2),
+          maxSpeed: maxSpeed,
+          location: {
+            latitude: reports[index - 1].latitude,
+            longitude: reports[index - 1].longitude,
+          },
+        });
+        distance = 0;
+        totalSpeed = 0;
+        speedCount = 0;
+        maxSpeed = 0;
+      }
+
+      if (engineOn && index > 0) {
+        const timeDiff = calculateDuration(reports[index - 1].time, entry.time);
+        distance += entry.speed * (timeDiff / 3600); // Time in hours
+
+        totalSpeed += entry.speed;
+        speedCount += 1;
+
+        // Track the maximum speed
+        if (entry.speed > maxSpeed) {
+          maxSpeed = entry.speed;
+        }
+      }
+    });
+    setEngineStats(stats);
+  }, [reports]);
 
   // Style for PDF Btn
-  const styles = {
-    flex: { width: "100%", display: "flex", gap: "5px", alignItems: "center" },
-    btn: {
-      borderRadius: "3px",
-      border: "1px solid gray",
-      display: "flex",
-      alignItems: "center",
-      gap: "2px",
-      padding: "3px",
-      fontSize: "11px",
-      color: "#4f4f4f",
-      fontWeight: 600,
-      cursor: "pointer",
-      userSelect: "none",
-    },
-  };
+  // const styles = {
+  //   flex: { width: "100%", display: "flex", gap: "5px", alignItems: "center" },
+  //   btn: {
+  //     borderRadius: "3px",
+  //     border: "1px solid gray",
+  //     display: "flex",
+  //     alignItems: "center",
+  //     gap: "2px",
+  //     padding: "3px",
+  //     fontSize: "11px",
+  //     color: "#4f4f4f",
+  //     fontWeight: 600,
+  //     cursor: "pointer",
+  //     userSelect: "none",
+  //   },
+  // };
 
-  const handleShare = async (blob) => {
-    await saveAs(blob, `invoice.pdf`);
-    window.location.href = `mailto:?subject=${encodeURIComponent(
-      `Invoice`,
-    )}&body=${encodeURIComponent(`Kindly find attached invoice`)}`;
-  };
+  // const handleShare = async (blob) => {
+  //   await saveAs(blob, `invoice.pdf`);
+  //   window.location.href = `mailto:?subject=${encodeURIComponent(
+  //     `Invoice`,
+  //   )}&body=${encodeURIComponent(`Kindly find attached invoice`)}`;
+  // };
 
   return (
     <>
@@ -85,7 +149,7 @@ console.log(reports);
             </dl>
           </div>
         </div>
-        <div className='flex justify-between items-center gap-8'>
+        {/* <div className='flex justify-between items-center gap-8'>
           <PDFDownloadLink
             document={
               <DistanceReportDocs
@@ -137,31 +201,71 @@ console.log(reports);
               </div>
             )}
           </BlobProvider>
-        </div>
+        </div> */}
       </div>
 
-      {reports.length > 0 ? (
+      {engineStats.length > 0 ? (
         <div className='overflow-x-auto rounded-lg border border-gray-200'>
           <table className='min-w-full divide-y-2 divide-gray-200 bg-white text-sm'>
             <thead className='ltr:text-left rtl:text-right'>
               <tr>
                 <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
-                  Date
+                  Time Range
                 </th>
                 <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
-                  Total Distance
+                  Duration
+                </th>
+
+                <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
+                  Travelled Distance
+                </th>
+                <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
+                  Avg. Speed
+                </th>
+                <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
+                  Max Speed
+                </th>
+                <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
+                  Location
+                </th>
+                <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
+                  Nearby Place
+                </th>
+                <th className='whitespace-nowrap px-4 py-2 font-medium text-gray-900'>
+                  Status
                 </th>
               </tr>
             </thead>
 
             <tbody className='divide-y divide-gray-200'>
-              {reports.map((report, index) => (
+              {engineStats.map((report, index) => (
                 <tr key={index}>
-                  <td className='whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900'>
-                    {report.date}
+                  <td className='whitespace-nowrap text-start px-4 py-2 font-medium text-gray-900'>
+                    <span>{report?.startTime}</span>
+                    <br />
+                    <span>{report?.stopTime}</span>
                   </td>
                   <td className='whitespace-nowrap px-4 text-center py-2 text-gray-700'>
-                    {report.totalDistance} km
+                    {report.duration}
+                  </td>
+                  <td className='whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900'>
+                    {report?.travelledDistance}km
+                  </td>
+
+                  <td className='whitespace-nowrap px-4 text-center py-2 text-gray-700'>
+                    {report.avgSpeed} km/h
+                  </td>
+                  <td className='whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900'>
+                    {report?.maxSpeed} km/h
+                  </td>
+                  <td className='whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900'>
+                    ({report?.location?.latitude},{report?.location?.longitude})
+                  </td>
+                  <td className='whitespace-nowrap text-center px-4 py-2 font-medium text-gray-900'>
+                    -
+                  </td>
+                  <td className='whitespace-nowrap px-4 text-center py-2 text-gray-700'>
+                    Off
                   </td>
                 </tr>
               ))}
