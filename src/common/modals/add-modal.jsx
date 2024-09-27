44801d1cee110a002e08a72380acb86344174836
kawsar007 <imkawsar007@gmail.com>
2024-09-27 17:53:59 +0600

    Reusable modal added

---
+++ src/common/modals/add-modal.jsx
@@ -0,0 +1,83 @@
+/* eslint-disable react/prop-types */
+import { useEffect, useRef, useState } from "react";
+import ModalCloseButton from "./modal-close-button";
+
+
+export default function AddModal(props) {
+  console.log(props);
+  
+  const { showModal, setShowModal, text } = props;
+  const [loading, setLoading] = useState(false);
+
+
+  // ===== click outside of dropdown =====
+  const divRef = useRef(null);
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const handleClickOutside = (event) => {
+        if (divRef.current && !divRef.current.contains(event.target)) {
+          setShowModal(false);
+        }
+      };
+
+      document.addEventListener("mousedown", handleClickOutside);
+      return () => {
+        document.removeEventListener("mousedown", handleClickOutside);
+      };
+    }
+  });
+
+  return (
+    <>
+      {showModal && (
+        <div
+          className={`py-7.6 fixed left-0 top-0 z-99999 flex h-screen w-full items-center justify-center bg-black/90 px-4 dark:bg-dark/70 sm:px-8 `}>
+          <div
+            ref={divRef}
+            className='shadow-7 relative h-auto max-h-[calc(100vh-60px)] w-full max-w-[900px] scale-100 transform overflow-y-auto rounded-[25px] bg-white transition-all dark:bg-black'>
+            <ModalCloseButton closeModal={setShowModal} />
+
+            <div className='flex flex-wrap gap-5.5 border-b border-stroke p-4 dark:border-stroke-dark sm:p-7.5 xl:p-10'>
+              <h3 className='mb-1.5 font-satoshi text-custom-2xl font-bold tracking-[-.5px] text-dark dark:text-white'>
+                Add User
+              </h3>
+
+              <form
+                className='w-full space-y-4'>
+                <div className='mt-2 items-center md:flex'>
+                  <div className='flex w-full flex-col md:w-1/2'>
+                    <div className='mb-4'>
+                      <label className='mb-2 block font-satoshi text-base font-medium text-dark dark:text-white'>
+                        Name
+                      </label>
+                      <input
+                        
+                        type='text'
+                        placeholder='Enter physician name'
+                        className='w-full rounded-lg border border-gray-3 px-5.5 py-3 text-dark outline-none dark:border-stroke-dark dark:bg-black dark:text-white'
+                      />
+                    </div>
+
+                    <div className='mb-4'>
+                      <label className='mb-2 block font-satoshi text-base font-medium text-dark dark:text-white'>
+                        Email Address
+                      </label>
+                      <input
+                        type='email'
+                        placeholder='jhon@gmail.com'
+                        className='w-full rounded-lg border border-gray-3 px-5.5 py-3 text-dark outline-none dark:border-stroke-dark dark:bg-black dark:text-white'
+                      />
+                    </div>
+                  </div>
+                </div>
+                <div className='mt-11'>
+                  <button>Submit</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+}
--- src/components/home/manager/index.jsx
+++ src/components/home/manager/index.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import { useState } from "react";
+import AddModal from "../../../common/modals/add-modal";
 
 const people = [
   {
@@ -84,106 +85,103 @@ const people = [
 ];
 
 export function Manager() {
+  const [showModal, setShowModal] = useState(false);
+  console.log(showModal);
+
   return (
     <>
-      <section className="mx-auto w-full py-4">
-        <div className="flex flex-col space-y-4 md:flex-row md:items-center md:justify-between md:space-y-0">
+      <section className='mx-auto w-full py-4'>
+        <div className='flex flex-col space-y-4 md:flex-row md:items-center md:justify-between md:space-y-0'>
           <div>
-            <h2 className="text-lg font-semibold">All Managers</h2>
-            <p className="mt-1 text-sm text-gray-700">
+            <h2 className='text-lg font-semibold'>All Managers</h2>
+            <p className='mt-1 text-sm text-gray-700'>
               This is a list of all managers. You can add new managers, edit or
               delete existing ones.
             </p>
           </div>
           <div>
             <button
-              type="button"
-              className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-            >
+              onClick={() => setShowModal(true)}
+              type='button'
+              className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'>
               Create Manager
             </button>
           </div>
         </div>
-        <div className="mt-6 flex flex-col">
-          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-              <div className="overflow-hidden border border-gray-200 md:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr className="divide-x divide-gray-200">
+        <div className='mt-6 flex flex-col'>
+          <div className='-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8'>
+            <div className='inline-block min-w-full py-2 align-middle md:px-6 lg:px-8'>
+              <div className='overflow-hidden border border-gray-200 md:rounded-lg'>
+                <table className='min-w-full divide-y divide-gray-200'>
+                  <thead className='bg-gray-50'>
+                    <tr className='divide-x divide-gray-200'>
                       <th
-                        scope="col"
-                        className="px-4 py-3.5 text-left text-sm font-normal text-gray-500"
-                      >
+                        scope='col'
+                        className='px-4 py-3.5 text-left text-sm font-normal text-gray-500'>
                         <span>Name</span>
                       </th>
                       <th
-                        scope="col"
-                        className="px-12 py-3.5 text-left text-sm font-normal text-gray-500"
-                      >
+                        scope='col'
+                        className='px-12 py-3.5 text-left text-sm font-normal text-gray-500'>
                         Address
                       </th>
 
                       <th
-                        scope="col"
-                        className="px-4 py-3.5 text-left text-sm font-normal text-gray-500"
-                      >
+                        scope='col'
+                        className='px-4 py-3.5 text-left text-sm font-normal text-gray-500'>
                         Contact
                       </th>
 
                       <th
-                        scope="col"
-                        className="px-4 py-3.5 text-left text-sm font-normal text-gray-500"
-                      >
+                        scope='col'
+                        className='px-4 py-3.5 text-left text-sm font-normal text-gray-500'>
                         Assigned Vehicle
                       </th>
-                      <th scope="col" className="relative px-4 py-3.5">
+                      <th scope='col' className='relative px-4 py-3.5'>
                         <span>Actions</span>
                       </th>
                     </tr>
                   </thead>
-                  <tbody className="divide-y divide-gray-200 bg-white">
+                  <tbody className='divide-y divide-gray-200 bg-white'>
                     {people.map((person) => (
                       <tr
                         key={person.name}
-                        className="divide-x divide-gray-200"
-                      >
-                        <td className="whitespace-nowrap px-4 py-4">
-                          <div className="flex items-center">
-                            <div className="h-10 w-10 flex-shrink-0">
+                        className='divide-x divide-gray-200'>
+                        <td className='whitespace-nowrap px-4 py-4'>
+                          <div className='flex items-center'>
+                            <div className='h-10 w-10 flex-shrink-0'>
                               <img
-                                className="h-10 w-10 rounded-full object-cover"
+                                className='h-10 w-10 rounded-full object-cover'
                                 src={person.image}
-                                alt=""
+                                alt=''
                               />
                             </div>
-                            <div className="ml-4">
-                              <div className="text-sm font-medium text-gray-900">
+                            <div className='ml-4'>
+                              <div className='text-sm font-medium text-gray-900'>
                                 {person.name}
                               </div>
                             </div>
                           </div>
                         </td>
-                        <td className="whitespace-nowrap px-12 py-4">
-                          <div className="text-sm text-gray-900">
+                        <td className='whitespace-nowrap px-12 py-4'>
+                          <div className='text-sm text-gray-900'>
                             {person.address}
                           </div>
                         </td>
-                        <td className="whitespace-nowrap px-4 py-4">
-                          <div className="text-sm text-gray-500">
+                        <td className='whitespace-nowrap px-4 py-4'>
+                          <div className='text-sm text-gray-500'>
                             {person.contact}
                           </div>
                         </td>
-                        <td className="whitespace-nowrap px-4 py-4">
-                          <div className="text-sm text-gray-500">
+                        <td className='whitespace-nowrap px-4 py-4'>
+                          <div className='text-sm text-gray-500'>
                             {person.assigned_vehicle}
                           </div>
                         </td>
-                        <td className="whitespace-nowrap px-4 py-4 text-right text-sm font-medium">
+                        <td className='whitespace-nowrap px-4 py-4 text-right text-sm font-medium'>
                           <a
-                            href="#"
-                            className="text-gray-500 hover:text-indigo-600"
-                          >
+                            href='#'
+                            className='text-gray-500 hover:text-indigo-600'>
                             View
                           </a>
                         </td>
@@ -195,25 +193,32 @@ export function Manager() {
             </div>
           </div>
         </div>
-        <div className="mt-4 w-full border-gray-300">
-          <div className="mt-2 flex items-center justify-end">
-            <div className="space-x-2">
+        <div className='mt-4 w-full border-gray-300'>
+          <div className='mt-2 flex items-center justify-end'>
+            <div className='space-x-2'>
               <button
-                type="button"
-                className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              >
+                type='button'
+                className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'>
                 &larr; Previous
               </button>
               <button
-                type="button"
-                className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
-              >
+                type='button'
+                className='rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black'>
                 Next &rarr;
               </button>
             </div>
           </div>
         </div>
       </section>
+
+      {showModal && (
+        <AddModal
+          showModal={showModal}
+          setShowModal={setShowModal}
+          text={"Add Manager"}
+          loading={false}
+        />
+      )}
     </>
   );
 }
