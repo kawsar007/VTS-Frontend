5815f7ba041343fcfeef253a1217a155472c2fb4
kawsar007 <imkawsar007@gmail.com>
2024-09-27 17:11:18 +0600

    Update .env

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const axiosOpen = axios.create({
   // eslint-disable-next-line no-undef
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json",
     Accept: "application/json",
